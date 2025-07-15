55708450dd5af8820a919fd5b182338bd60fc6e7
Connor van Spronssen <info@dobefu.com>
2025-07-15 07:25:39 +0200

    feat: Add a button

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"image/color"
 	"log"
 
-	"golang.org/x/image/font/gofont/goregular"
-
 	"github.com/ebitenui/ebitenui"
+	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 type game struct {
@@ -18,37 +19,72 @@ type game struct {
 }
 
 func main() {
-	ebiten.SetWindowSize(400, 400)
-	ebiten.SetWindowTitle("Ebiten UI canvas scaling")
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	buttonImage, _ := loadButtonImage()
 
-	rootContainer := widget.NewContainer()
+	face, _ := loadFont(20)
+
+	rootContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(
+			widget.NewAnchorLayout(),
+		),
+	)
 
 	ui := &ebitenui.UI{
 		Container: rootContainer,
 	}
 
-	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
-	if err != nil {
-		log.Fatal(err)
-	}
+	button := widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter,
+			}),
+		),
+		widget.ButtonOpts.Image(buttonImage),
 
-	var fontFace text.Face = &text.GoTextFace{
-		Source: s,
-		Size:   32,
-	}
+		widget.ButtonOpts.Text("Button :D", face, &widget.ButtonTextColor{
+			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+		widget.ButtonOpts.TextProcessBBCode(false),
+		widget.ButtonOpts.TextPadding(widget.Insets{
+			Left:   30,
+			Right:  30,
+			Top:    5,
+			Bottom: 5,
+		}),
 
-	helloWorldLabel := widget.NewText(
-		widget.TextOpts.Text("Hello World!", fontFace, color.White),
+		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
+			println("button clicked")
+		}),
+
+		widget.ButtonOpts.CursorEnteredHandler(func(args *widget.ButtonHoverEventArgs) {
+			println("cursor entered button: entered =", args.Entered, "offsetX =", args.OffsetX, "offsetY =", args.OffsetY)
+		}),
+
+		widget.ButtonOpts.CursorEnteredHandler(func(args *widget.ButtonHoverEventArgs) {
+			println("cursor entered button: entered =", args.Entered, "offsetX =", args.OffsetX, "offsetY =", args.OffsetY)
+		}),
+
+		widget.ButtonOpts.CursorMovedHandler(func(args *widget.ButtonHoverEventArgs) {
+			println("cursor moved on button: entered =", args.Entered, "offsetX =", args.OffsetX, "offsetY =", args.OffsetY, "diffX =", args.DiffX, "diffY =", args.DiffY)
+		}),
+
+		widget.ButtonOpts.CursorExitedHandler(func(args *widget.ButtonHoverEventArgs) {
+			println("cursor exited button: entered =", args.Entered, "offsetX =", args.OffsetX, "offsetY =", args.OffsetY)
+		}),
 	)
 
-	rootContainer.AddChild(helloWorldLabel)
+	rootContainer.AddChild(button)
+
+	ebiten.SetWindowSize(400, 400)
+	ebiten.SetWindowTitle("Ebiten UI canvas scaling")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
 	game := game{
 		ui: ui,
 	}
 
-	err = ebiten.RunGame(&game)
+	err := ebiten.RunGame(&game)
 	if err != nil {
 		log.Print(err)
 	}
@@ -66,4 +102,32 @@ func (g *game) Draw(screen *ebiten.Image) {
 
 func (g *game) Layout(outsideWidth int, outsideHeight int) (int, int) {
 	return outsideWidth, outsideHeight
+}
+
+func loadButtonImage() (*widget.ButtonImage, error) {
+
+	idle := image.NewBorderedNineSliceColor(color.NRGBA{R: 170, G: 170, B: 180, A: 255}, color.NRGBA{90, 90, 90, 255}, 3)
+
+	hover := image.NewBorderedNineSliceColor(color.NRGBA{R: 130, G: 130, B: 150, A: 255}, color.NRGBA{70, 70, 70, 255}, 3)
+
+	pressed := image.NewAdvancedNineSliceColor(color.NRGBA{R: 130, G: 130, B: 150, A: 255}, image.NewBorder(3, 2, 2, 2, color.NRGBA{70, 70, 70, 255}))
+
+	return &widget.ButtonImage{
+		Idle:    idle,
+		Hover:   hover,
+		Pressed: pressed,
+	}, nil
+}
+
+func loadFont(size float64) (text.Face, error) {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
+	if err != nil {
+		log.Fatal(err)
+		return nil, fmt.Errorf("Error loading font: %w", err)
+	}
+
+	return &text.GoTextFace{
+		Source: s,
+		Size:   size,
+	}, nil
 }
